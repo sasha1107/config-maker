7d61ffbf3a7c7258a36d54d3bba312366854e93b
정수현 <sasha1107@naver.com>
2024-02-27 16:04:11 +0900

    style: color chip 수정

--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,13 +1,16 @@
-import Install from './_components/Install';
-import { useLocation } from 'react-router-dom';
-import { Code } from '@components';
+import Install from "./_components/Install";
+import { useLocation } from "react-router-dom";
+import { Code } from "@components";
 
 const Result = () => {
   const { state } = useLocation();
   return (
     <div>
-      <Install />
-      <Code copy>{JSON.stringify(Object.fromEntries(state), null, 2)}</Code>
+      <h2 className="text-3xl text-center mb-10">결과</h2>
+      <div className="flex flex-col gap-8">
+        <Install />
+        <Code copy>{JSON.stringify(Object.fromEntries(state), null, 2)}</Code>
+      </div>
     </div>
   );
 };
